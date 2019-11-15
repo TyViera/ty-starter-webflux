096b5dc22dbfd61c6b90156427d9517cce9bf4b1
Nazaret Viera Ipanaque <nazaret.vieraipanaque@tcs.com>
2019-11-14 19:03:32 -0500

    feat: initial configuration chore: add dependencies

---
+++ src/main/java/pe/ty/tystarterwebflux/TyStarterWebfluxApplication.java
@@ -0,0 +1,13 @@
+package pe.ty.tystarterwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyStarterWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TyStarterWebfluxApplication.class, args);
+	}
+
+}
